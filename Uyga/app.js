11f30a4f2c0be98bef89d728d10043385c5813f1
Zuhriddin1 <anarimon1985@gmail.com>
2023-11-23 20:28:33 +0300

    first commit

---
+++ Uyga/app.js
@@ -0,0 +1,18 @@
+// let a = prompt("Son");
+// let b = prompt("Son");
+// function qoldilk(a, b) {
+//   return a * b;
+// }
+// console.log(qoldilk());
+// let boyi = +prompt(`Boyini toping`);
+// let eni = +prompt(`Enini toping`);
+// function Turbuchak(boyi, eni) {
+//   return boyi * eni;
+// }
+// console.log(Turbuchak());
+// let soz = prompt("soz");
+// let a = prompt("soz");
+// function stringQoshimcha(a) {
+//   a + soz;
+// }
+// alert(stringQoshimcha());
---
+++ main.js
@@ -0,0 +1,23 @@
+// let soz = prompt("Soz kiriting");
+// let qop =   0;
+// for (let i = 0; i < soz.length; i++) {
+//   if (soz[i] == "a" || soz[i] == "A") {
+//     qop++;
+//   }
+// }
+// console.log(qop);
+// let soz = prompt("Ismingiz nima");
+// if (soz.length > 12) {
+//   alert("Salom " + soz.slice(13));
+// }else{
+//     alert("Salom " + soz)
+// }
+let word = prompt("Soz iltimos kiriting");
+let bogin = "";
+for (let i = 0; i < word.length; i++) {
+  if (word.length == "") {
+    alert("Salom");
+  } else {
+    alert("Fuck");
+      }
+}
